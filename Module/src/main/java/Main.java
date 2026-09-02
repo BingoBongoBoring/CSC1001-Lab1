75c68984368532c5f01f05ac2111df54e262070c
Bingo <business@rdstudios.org>
2026-09-02 16:04:47 -0700

    wrote the decryptors

--- Module/src/main/java/Main.java
+++ Module/src/main/java/Main.java
@@ -1,6 +1,47 @@
+import java.nio.charset.StandardCharsets;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    public static String decryptorA(String s) {
+        return s.substring(1, s.length() - 1);
+    }
+
+    public static String decryptorB(String s) {
+        return s.charAt(s.length() - 1) + s.substring(1, s.length() - 1) + s.charAt(0);
+    }
+
+    public static String decryptorC(String s) {
+        return s.charAt(s.length() - 3) + s.substring(0, s.length() - 3);
+    }
+
+    public static String decryptorD(String s) {
+        int i = Math.floorDiv(s.length(), 2);
+        return s.substring(0, i) + s.charAt(s.length() - 1) + s.substring(i, s.length() - 1);
+    }
+
+    public static String decryptorLA(String s) {
+        int i = Math.ceilDiv(s.length(), 2);
+        return s.substring(i, s.length()) + (new StringBuilder(s.substring(0, i))).reverse();
+    }
+
+    public static String decryptorLB(String s) {
+        StringBuilder r = new StringBuilder();
+        char[] a = new char[s.length()];
+        s.getChars(0, s.length(), a, 0);
+        for (char b : a) {
+            switch (b) {
+            case 'a': r.append("u"); break;
+            case 'o': r.append("i"); break;
+            case 'e': r.append("a"); break;
+            case 'u': r.append("o"); break;
+            case 'i': r.append("e"); break;
+            default: r.append(b); break;
+            }
+        }
+        return r.toString();
+    }
+
     // add six to a number
     public static int addSix(int a){
         return a + 6;
--- Module/src/test/java/MainTest.java
+++ Module/src/test/java/MainTest.java
@@ -11,4 +11,65 @@ class MainTest {
 
     @Test
     void addSeventeen() { assertEquals(18, Main.addSeventeen(1)); }
+
+    @Test
+    void decryptorA() {
+        assertEquals("miasma", Main.decryptorA("*miasma*"));
+        assertEquals("michael", Main.decryptorA("*michael*"));
+        assertEquals("hello there everybody", Main.decryptorA("*hello there everybody*"));
+    }
+    @Test
+    void decryptorB() {
+        assertEquals("miasma", Main.decryptorB("aiasmm"));
+        assertEquals("mantelpiece", Main.decryptorB("eantelpiecm"));
+        assertEquals("hello there everybody", Main.decryptorB("yello there everybodh"));
+    }
+    @Test
+    void decryptorC() {
+        assertEquals("miasma", Main.decryptorC("iasmamay"));
+        assertEquals("mantelpiece", Main.decryptorC("antelpiecemay"));
+        assertEquals("largesse", Main.decryptorC("argesselay"));
+        assertEquals("hello there everybody", Main.decryptorC("ello there everybodyhay"));
+    }
+    @Test
+    void decryptorD() {
+        assertEquals("miasma", Main.decryptorD("miamas"));
+        assertEquals("mantelpiece", Main.decryptorD("mantepiecel"));
+        assertEquals("largesse", Main.decryptorD("largssee"));
+        assertEquals("hello there everybody", Main.decryptorD("hello ther everybodye"));
+        assertEquals("antediluvian", Main.decryptorD("antediuvianl"));
+        assertEquals("hi", Main.decryptorD("hi"));
+    }
+    @Test
+    void decryptorLA() {
+        assertEquals("miasma", Main.decryptorLA("amsmia"));
+        assertEquals("mantelpiece", Main.decryptorLA("eceiplmante"));
+        assertEquals("largesse", Main.decryptorLA("esselarg"));
+        assertEquals("hello there everybody", Main.decryptorLA("ydobyreve ehello ther"));
+        assertEquals("antediluvian", Main.decryptorLA("naivulantedi"));
+        assertEquals("hi", Main.decryptorLA("ih"));
+        assertEquals("whisker pole", Main.decryptorLA("elop rwhiske"));
+        assertEquals("広島市（ひろしまし）は、広島県の西部、安芸地方にある市。政令指定都市・中枢中核都市に指定されている。", Main.decryptorLA("。るいてれさ定指に市都核中枢中・市都定指令政。市る広島市（ひろしまし）は、広島県の西部、安芸地方にあ"));
+    }
+    @Test
+    void decryptorLB() {
+        assertEquals("miasma", Main.decryptorLB("moesme"));
+        assertEquals("mantelpiece", Main.decryptorLB("mentilpoici"));
+        assertEquals("largesse", Main.decryptorLB("lergissi"));
+        assertEquals("hello there everybody", Main.decryptorLB("hillu thiri ivirybudy"));
+        assertEquals("antediluvian", Main.decryptorLB("entidolavoen"));
+        assertEquals("whisker pole", Main.decryptorLB("whoskir puli"));
+        assertEquals("広島市（ひろしまし）は、広島県の西部、安芸地方にある市。政令指定都市・中枢中核都市に指定されている。", Main.decryptorLB("広島市（ひろしまし）は、広島県の西部、安芸地方にある市。政令指定都市・中枢中核都市に指定されている。"));
+    }
+//    @Test
+//    void decryptorLC() {
+//        assertEquals("miasma", Main.decryptorLC("maasmi"));
+//        assertEquals("mantelpiece", Main.decryptorLC("mcnielpteae"));
+//        assertEquals("largesse",
+//             Main.decryptorLC("lersegsa"));
+//        assertEquals("hello there everybody",
+//             Main.decryptorLC("hdlbortve erehe yloey"));
+//        assertEquals("antediluvian", Main.decryptorLC("antidulivean"));
+//        assertEquals("whisker pole", Main.decryptorLC("weiok repslh"));
+//    }
 }
